deafd9c9b102789154d9e882253dca80f981afab
Ankit patel <36583951+4nkitpatel@users.noreply.github.com>
2020-07-14 22:50:11 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAjw68uob5haCwQpBczGBdYoHGPrXd-iag";
+const KEY = "YOUR_YOUTUBE_API_KEY";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
